2e3d823b090f78a1e21b22ad470f8eaea16956e6
David Silva <david@davidsilva.pt>
2018-09-22 22:43:32 +0100

    Modificado hook

--- library.js
+++ library.js
@@ -29,8 +29,12 @@
 
   // If this filter is not there, the deleteUserData function will fail when getting the oauthId for deletion.
   TudoPreso.invokeBot = function(params, callback) {
+	var notification = params.notification,
+	var uids = params.uids;	
 	console.log('params', params);
 	console.log('callback', callback);
+	console.log('notification', notification);
+	console.log('uids', uids);
     callback(null, params);
   };
 
